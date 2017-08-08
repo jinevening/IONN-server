f7c333ee367e7efd0243bf83cccce4ee3a0ecf95
hjj <hjj@altair.snu.ac.kr>
2017-08-08 19:27:55 +0900

    Partitioning algorithm implementation
      NN execution graph, shortestPath are implemented
      for optimizing execution time, energy consumption

---
+++ test.sh
@@ -0,0 +1,7 @@
+./build/examples/partitioning/classification.bin \
+  models/bvlc_googlenet/deploy.prototxt \
+  models/bvlc_googlenet/bvlc_googlenet.caffemodel \
+  data/ilsvrc12/imagenet_mean.binaryproto \
+  data/ilsvrc12/synset_words.txt \
+  examples/images/cat.jpg
+
